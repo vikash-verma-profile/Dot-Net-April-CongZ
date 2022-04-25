974c65a04dc3ad3dade4e1883af900b857cdeeaa
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-04-25 11:57:26 +0530

    first commit

---
+++ EcmmerceWebApi/EcmmerceWebApi/Models/EcommerceDbsContext.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace EcmmerceWebApi.Models
+{
+    public partial class EcommerceDbsContext : DbContext
+    {
+        public EcommerceDbsContext()
+        {
+        }
+
+        public EcommerceDbsContext(DbContextOptions<EcommerceDbsContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<TblCategory> TblCategories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=EcommerceDbs;Trusted_connection=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<TblCategory>(entity =>
+            {
+                entity.ToTable("tblCategory");
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.CategoryName)
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.CreateOn).HasColumnType("date");
+
+                entity.Property(e => e.ParentId).HasColumnName("ParentID");
+
+                entity.Property(e => e.UpdatedOn).HasColumnType("date");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}

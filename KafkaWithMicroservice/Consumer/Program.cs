d7ccbbb807fda8fbc7e6c920200f6c967355e48c
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-05-05 12:08:36 +0530

    first commit

---
+++ KafkaWithMicroservice/Consumer/Program.cs
@@ -0,0 +1,28 @@
+﻿using Confluent.Kafka;
+using System;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConsumerConfig
+            {
+                GroupId="gid-consumers",
+                BootstrapServers="localhost:9092"
+            };
+            using (var consumer=new ConsumerBuilder<Null, string>(config).Build())
+            {
+                consumer.Subscribe("temp-topic-cf");
+                while (true)
+                {
+                    var cr = consumer.Consume();
+                    Console.WriteLine(cr.Message.Value);
+                }
+               
+            }
+                
+        }
+    }
+}

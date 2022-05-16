bf0a171aed158e64ca965c258cbe2aa819c0c216
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-05-16 16:57:25 +0530

    first commit

---
+++ Angular-Demos/cookie-store/src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { CookieService } from 'ngx-cookie';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'cookie-store';
+  cookieValue?: string;
+  objectCookieValue?: object;
+  hasCookieTrue!: boolean;
+  hasCookieFalse!: boolean;
+
+  private key = "myCookie";
+  private objectKey = 'myObjectCookie';
+  constructor(private cookieService: CookieService) {
+  }
+
+  setCookies(): void {
+    this.cookieService.put(this.key, 'myValue');
+    this.cookieService.putObject(this.objectKey, { myKey: 'myValue' });
+  }
+
+  getCookies():void{
+    this.cookieValue=this.cookieService.get(this.key);
+    this.objectCookieValue=this.cookieService.getObject(this.objectKey);
+
+    this.hasCookieTrue=this.cookieService.hasKey(this.key) && this.cookieService.hasKey(this.objectKey);
+    this.hasCookieFalse=this.cookieService.hasKey('nonExistentKey');
+  }
+}
